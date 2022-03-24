b0c81cc5082fe115319863d636999bd1ba58c42a
Leilani Battle <leibatt@gmail.com>
2022-03-24 15:02:35 -0700

    adding index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import * as insight from './insight';
 import * as knowledge from './knowledge';
 import * as dataset from './dataset';
 import * as util from './util';
+import * as relationship from './relationship';
 
 import {version} from '../package.json';
 
-export { dataset, knowledge, insight };
+export { dataset, knowledge, insight, relationship, version };
+
+//relationship	transformation	util.ts
