8ceed349ad5d9c91a74e82f6201664802807fb58
Leilani Battle <leibatt@gmail.com>
2022-03-25 11:44:10 -0700

    updating index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import * as knowledge from './knowledge';
 import * as dataset from './dataset';
 import * as util from './util';
 import * as relationship from './relationship';
+import * as transformation from './transformation';
 
-import {version} from '../package.json';
+import { version } from '../package.json';
 
-export { dataset, knowledge, insight, relationship, version };
+export { dataset, knowledge, insight, relationship, transformation, util, version };
 
-//relationship	transformation	util.ts
---
+++ src/transformation.ts
@@ -0,0 +1,5 @@
+import { DataTransformation } from './transformation/DataTransformation';
+import * as arquero from './transformation/Arquero';
+import * as vega from './transformation/VegaTransform';
+
+export { DataTransformation, arquero, vega };
