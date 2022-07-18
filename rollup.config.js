cdf92b37f44162af8ccf69f71b009f7f0bc63de4
Leilani Battle <leibatt@cs.washington.edu>
2022-07-18 13:26:52 -0700

    support ES6 and commojs outputs

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default {
+export default [
+{
   input: 'src/index.ts',
   output: {
     sourcemap: true,
-    dir: 'output',
+    dir: 'output/es',
+    format: 'es'
+  },
+  plugins: [
+    typescript({outDir: 'output/es'}),
+    commonjs({transformMixedEsModules:true})
+  ]
+},
+{
+  input: 'src/index.ts',
+  output: {
+    sourcemap: true,
+    dir: 'output/cjs',
     format: 'cjs'
   },
   plugins: [
-    typescript({outDir: 'output'}),
+    typescript({outDir: 'output/cjs'}),
     commonjs({transformMixedEsModules:true})
   ]
-};
+}
+];
