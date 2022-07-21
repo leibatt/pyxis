3197bfa3247c4661c9a3934d15facd2a760a89ad
Leilani Battle <leibatt@cs.washington.edu>
2022-07-21 10:49:00 -0700

    tidying

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default [
-{
+export default [{
   input: 'src/index.ts',
+  external: ['fs', 'path', 'uuid', 'arquero', 'vega', 'ml-regression-multivariate-linear', 'isolation-forest', 'ml-distance-euclidean', 'ml-matrix', 'ml-array-mean'],
   output: {
     sourcemap: true,
     dir: 'build',
@@ -13,5 +13,5 @@ export default [
     typescript({outDir: 'build'}),
     commonjs({transformMixedEsModules:true})
   ]
-}
-];
+}];
+
