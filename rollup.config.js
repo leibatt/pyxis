4d2bfa000312298c3987e0c1fa6c56c1152c2a55
Leilani Battle <leibatt@cs.washington.edu>
2022-07-25 12:53:51 -0700

    making umd build separate and cleaning up dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,41 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
-export default [{
+export default [
+{
   input: 'src/index.ts',
   external: ['fs', 'path', 'uuid', 'arquero', 'vega', 'ml-regression-multivariate-linear', 'isolation-forest', 'ml-distance-euclidean', 'ml-matrix', 'ml-array-mean'],
   output: {
     sourcemap: true,
     dir: 'build',
-    format: 'umd',
-    name: 'pyxis'
+    format: 'cjs'
   },
   plugins: [
     typescript({outDir: 'build'}),
     commonjs({transformMixedEsModules:true})
+  ]
+},
+{
+  input: 'src/index.ts',
+  external: ['fs', 'path', 'uuid', 'arquero', 'vega', 'ml-regression-multivariate-linear', 'isolation-forest', 'ml-distance-euclidean', 'ml-matrix', 'ml-array-mean'],
+  output: {
+    sourcemap: true,
+    dir: 'umd',
+    format: 'umd',
+    name: 'pyxis',
+    globals: {
+      arquero: 'aq',
+      fs: 'fs',
+      path: 'path',
+      uuid: 'uuid',
+      vega: 'vega'
+    }
+  },
+  plugins: [
+    typescript({outDir: 'umd'}),
+    commonjs({transformMixedEsModules:true}),
+    nodePolyfills({ include: null, sourceMap: true })
   ]
 }];
 
