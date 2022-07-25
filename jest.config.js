a589f6e7c8668bf2b4ad13743d86bbd010eff58f
Leilani Battle <leibatt@cs.washington.edu>
2022-07-25 13:31:32 -0700

    upgrade vega and cleanup jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/build','<rootDir>/build-test','<rootDir>/umd', `/node_modules/(?!vega-label)`]
+  modulePathIgnorePatterns: ['<rootDir>/build','<rootDir>/build-test','<rootDir>/umd']
 };
